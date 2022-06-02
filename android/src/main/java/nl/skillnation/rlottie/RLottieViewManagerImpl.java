05b6d73f320678f2ace0c9bc2e391969b26e88d5
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-06-02 17:19:40 +0200

    chore(android): unify code base

---
+++ android/src/main/java/nl/skillnation/rlottie/RLottieViewManagerImpl.java
@@ -0,0 +1,62 @@
+package nl.skillnation.rlottie;
+
+import androidx.annotation.NonNull;
+
+import com.aghajari.rlottie.AXrLottie;
+import com.aghajari.rlottie.AXrLottieDrawable;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.SimpleViewManager;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+public class RLottieViewManagerImpl extends SimpleViewManager<RLottieView> {
+
+    public static final String NAME = "RLottieView";
+
+    protected ReactApplicationContext mCallerContext;
+
+    public RLottieViewManagerImpl(ReactApplicationContext reactContext) {
+        mCallerContext = reactContext;
+        AXrLottie.init(reactContext);
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @NonNull
+    @Override
+    protected RLottieView createViewInstance(@NonNull ThemedReactContext context) {
+        return new RLottieView(context);
+    }
+
+    @ReactProp(name="isAutoPlay")
+    public void setIsAutoPlay(RLottieView view, boolean isAutoPlay) {
+        view.isAutoPlay = isAutoPlay;
+    }
+
+    @ReactProp(name="src")
+    public void setSrc(RLottieView view, String jsonString) {
+        view.setLottieDrawable(
+                AXrLottieDrawable
+                        .fromJson(jsonString, "cacheKeyTodo")
+                        .setSize(700, 700)
+                        .build()
+        );
+
+        if (view.isAutoPlay) view.playAnimation();
+    }
+
+    @ReactProp(name="progress")
+    public void setProgress(RLottieView view, float progress) {
+        view.getLottieDrawable().setProgress(progress);
+    }
+
+    @Override
+    public void onDropViewInstance(@NonNull RLottieView view) {
+        super.onDropViewInstance(view);
+        view.release();
+    }
+}
--- android/src/oldarch/java/nl/skillnation/rlottie/RLottieViewManager.java
+++ android/src/oldarch/java/nl/skillnation/rlottie/RLottieViewManager.java
@@ -1,60 +1,9 @@
 package nl.skillnation.rlottie;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.aghajari.rlottie.AXrLottie;
-import com.aghajari.rlottie.AXrLottieDrawable;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.SimpleViewManager;
-import com.facebook.react.uimanager.ThemedReactContext;
-import com.facebook.react.uimanager.annotations.ReactProp;
 
-public class RLottieViewManager extends SimpleViewManager<RLottieView> {
-    public static final String REACT_CLASS = "RLottieView";
-    protected ReactApplicationContext mCallerContext;
-
+public class RLottieViewManager extends RLottieViewManagerImpl {
     public RLottieViewManager(ReactApplicationContext reactContext) {
-        mCallerContext = reactContext;
-        AXrLottie.init(reactContext);
-    }
-
-    @Override
-    public String getName() {
-        return REACT_CLASS;
-    }
-
-    @NonNull
-    @Override
-    protected RLottieView createViewInstance(@NonNull ThemedReactContext reactContext) {
-        return new RLottieView(reactContext);
-    }
-
-    @ReactProp(name="isAutoPlay")
-    public void setIsAutoPlay(RLottieView view, boolean isAutoPlay) {
-        view.isAutoPlay = isAutoPlay;
-    }
-
-    @ReactProp(name="src")
-    public void setSrc(RLottieView view, String jsonString) {
-        view.setLottieDrawable(
-                AXrLottieDrawable
-                        .fromJson(jsonString, "cacheKeyTodo")
-                        .setSize(700, 700)
-                        .build()
-        );
-
-        if (view.isAutoPlay) view.playAnimation();
-    }
-
-    @ReactProp(name="progress")
-    public void setProgress(RLottieView view, float progress) {
-        view.getLottieDrawable().setProgress(progress);
-    }
-
-    @Override
-    public void onDropViewInstance(@NonNull RLottieView view) {
-        super.onDropViewInstance(view);
-        view.release();
+        super(reactContext);
     }
 }
