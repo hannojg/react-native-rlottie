576c4264449c8415e37580b55e186860f64fa0ae
Hanno J. Gödecke <die.drei99@yahoo.de>
2022-06-07 13:01:02 +0200

    performance measure rlottie

--- example/src/screens/PerformanceRLottieScreen.tsx
+++ example/src/screens/PerformanceRLottieScreen.tsx
@@ -1,41 +1,7 @@
 import React from 'react';
-import { useState } from 'react';
-import { Button } from 'react-native';
-import {View, StyleSheet, Dimensions} from 'react-native';
 import RLottieView from 'react-native-rlottie';
-import PerformanceStats from "react-native-performance-stats";
-
-const anim = require('../../assets/icon_trophy.json');
+import {PerformanceScreen} from '../components/PerformanceScreen';
 
 export const PerformanceRLottieScreen = () => {
-    const [showAnim, setShowAnim] = useState(false);
-
-    const onPressStart = () => {
-        PerformanceStats.addListener((data) => {
-            // data.;
-        })
-
-        setTimeout(() => {
-            setShowAnim(true);
-        }, 3000);
-    }
-
-    return (
-        <View style={styles.container}>
-            <Button title="Start test" onPress={onPressStart} />
-            {showAnim && <RLottieView source={anim} autoPlay style={styles.anim} />}
-        </View>
-    );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  anim: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').width,
-  },
-});
+  return <PerformanceScreen LottieComponent={RLottieView} />;
+};
